bf9f59cc55a5f5ecbaa603c8f16d6dd04db1986f
Soh Jun Wen <junwen.soh@theoptimum.net>
2023-07-11 10:36:11 +0800

    Fun with Widgets

--- pages/widgets.py
+++ pages/widgets.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
+
+def on_change_handler():
+    st.session_state.select_video = "is_selected"
+
+
 # variable name can be what the slider input represents
-some_number = st.slider("test slider")
+some_number = st.slider("test slider", value=[50, 60])
 
 st.write(some_number)
+
+selected_video = st.selectbox(
+    "Please select a video.",
+    options=["Video A", "Video B", "Video C"],
+)
+
+st.write(selected_video)
